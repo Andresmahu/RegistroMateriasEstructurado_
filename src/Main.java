72279808e175ae767f073f34855382011c2cfde6
Andrés <andres.m.hurtado2907@gmail.com>
2023-02-07 08:49:27 -0500

    Adelanto 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args) {
 
-        int[] Materias;
+    public static void main(String[] args) {
+        String[] NombreMaterias;
         int[] Estudiantes;
         int[] Grupos;
-
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de Materias que verá");
+        int longitud = teclado.nextInt();
+        NombreMaterias=new String[longitud];
+        for (int i=0;i<NombreMaterias.length;i++){
+            NombreMaterias[i]=teclado.next();
+        }
     }
 }
