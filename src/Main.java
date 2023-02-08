46be82664193a04f629640a1aba5358f07585042
Andrés <andres.m.hurtado2907@gmail.com>
2023-02-08 18:20:51 -0500

    Adelanto 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        String[] NombreMaterias;
-        int[] Estudiantes;
-        int[] Grupos;
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingrese la cantidad de Materias que verá");
-        int longitud = teclado.nextInt();
-        NombreMaterias=new String[longitud];
-        for (int i=0;i<NombreMaterias.length;i++){
-            NombreMaterias[i]=teclado.next();
+        Scanner teclado = new Scanner (System.in);
+        String Carreras[]={"Ciencias de la Computación e Inteligencia Artificial","Diseño Digital"};
+        String Materias[][]={{"Calculo Integral","Algebra Lineal","Cibernética"},{"E-Bussines","Taller de Ilustración","Taller de Algoritmia"}};
+        int Grupos[][]={{1,2,3},{23,30,34},{30,32,27},{35,36,34}};
+        System.out.println("Elija una opción para ingresar al sistema");
+        System.out.println("1.Funcionario");
+        System.out.println("2.Estudiante");
+        int opcion = teclado.nextInt();
+        if(opcion!=1&&opcion!=2){
+            System.out.println("Ingrese una opción valida");
+            opcion = teclado.nextInt();
         }
+        if (opcion==1){
+            System.out.println("Ingrese su usuario y contraseña");
+            System.out.println("Usuario:");
+            String usuario = teclado.next();
+            System.out.println("Contraseña:");
+            String contraseña = teclado.next();
+            boolean funcionamiento;
+            funcionamiento= true;
+            while(funcionamiento==true){
+                System.out.println("Que desea hacer?");
+                System.out.println("1.Crear una carrera");
+                System.out.println("2.Crear una materia");
+                System.out.println("3.Crear un grupo");
+                System.out.println("Cuantas carreras desea crear");
+                int longitud = teclado.nextInt();
+                Carreras= new String[longitud];
+
+            }
+        }
+        if (opcion==2){
+
+        }
+
+
+
     }
 }
